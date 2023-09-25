2d0562a42e41e994b488ec957d37ea0b07906a11
ekelly <masterevank@gmail.com>
2023-09-25 15:43:54 +1000

    Actually add build folder

---
+++ .output/public/_nuxt/default.155701af.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c as n,A as s}from"./entry.6d4ae5d6.js";const a={};function r(e,c,l,_,f,p){return o(),n("main",null,[s(e.$slots,"default")])}const u=t(a,[["render",r]]);export{u as default};
---
+++ .output/server/chunks/app/styles.mjs
@@ -0,0 +1,10 @@
+const interopDefault = r => r.default || r || [];
+const styles = {
+  "pages/index.vue": () => import('./_nuxt/index-styles.f069b89c.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.cf00f4cc.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.a001b7ed.mjs').then(interopDefault),
+  "layouts/default.vue": () => import('./_nuxt/default-styles.2a62ad44.mjs').then(interopDefault)
+};
+
+export { styles as default };
+//# sourceMappingURL=styles.mjs.map
