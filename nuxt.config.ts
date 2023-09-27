8cd3dd3848849b460b82d638dddac02e57e5372f
ekelly <masterevank@gmail.com>
2023-09-27 12:49:49 +1000

    Add og:image to nixy config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,11 @@ export default defineNuxtConfig({
   scss: ['~/assets/global.scss'],
   buildModules: ['@nuxtjs/sass'],
   head: {
+    meta: [
+      { hid: 'og-image', property: 'og:image', content: 'https://pennyprojects.org/images/og-image.webp' },
+    ],
     htmlAttrs: {
       lang: 'en'
-    }
+    },
   }
 })
