e80bc871c9dd10977b901e2a05f710ece69902e9
ekelly <masterevank@gmail.com>
2023-10-01 17:20:09 +1100

    Update og:image property

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   buildModules: ['@nuxtjs/sass'],
   head: {
     meta: [
-      { hid: 'og-image', property: 'og:image', content: 'https://pennyprojects.org/images/og-image.webp' },
+      { hid: 'og-image', property: 'og:image', content: 'https://pennyprojects.org/images/og-image.png' },
     ],
     htmlAttrs: {
       lang: 'en'
